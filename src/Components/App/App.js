5d5ca05e93181df0cd0544c704f5e9963c4f787e
Kate McFaul <katemcfaul@gmail.com>
2017-12-19 19:04:54 -0800

    Header

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Header } from '../Header/Header';
 
+
+// Home of the Illness info
 class App extends Component {
+  state = {
+    illnesses: [
+      {
+        id: 63790,
+        name: `Baby's Flu`,
+        records: [
+          {
+            id: 35890,
+            info: 'temp 103.1',
+            date: new Date(2017, 11, 17, 16, 0)
+          },
+          {
+            id: 24567,
+            info: 'tylenol',
+            date: new Date(2017, 11, 18, 15, 15)
+          }
+        ]
+      }
+    ]
+  }
+
   render() {
     return (
-      <p>Hello World</p>
+      <Header />
     );
   }
 }
